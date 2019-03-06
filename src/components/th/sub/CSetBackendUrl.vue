72afb430bc44133e0f04dd0c7c361e1121d95ba9
chrXiTer <1026336374@qq.com>
2019-03-06 09:05:00 +0800

    123
    123

--- src/components/th/sub/CSetBackendUrl.vue
+++ src/components/th/sub/CSetBackendUrl.vue
@@ -5,6 +5,15 @@
       <el-button slot="append" icon="el-icon-check" v-on:click="chgRootUrl">
         {{isDiff}}</el-button>
     </el-input>
+
+
+    <el-autocomplete
+      class="inline-input"
+      v-model="newBackendUrl"
+      :fetch-suggestions="querySearch"
+      placeholder="请输入内容"
+      @select="handleSelect"
+    ></el-autocomplete>
 </div>
 </template>
 
@@ -14,7 +23,8 @@ export default {
   data () {
     return {
       newBackendUrl: this.backendUrl,
-      isDiff: ''
+      isDiff: '',
+      restaurants:[this.backendUrl, 'http://localhost:18100']
     }
   },
   computed:{
@@ -27,8 +37,19 @@ export default {
     chgRootUrl(){
       this.$emit('chgBackendUrl',this.newBackendUrl)     
       this.isDiff = ""
-    }
+    },
 
+
+      querySearch(queryString, cb) {
+        var restaurants = this.restaurants;
+        var results = queryString ? restaurants.filter(this.createFilter(queryString)) : restaurants;
+        cb(results); // 调用 callback 返回建议列表的数据
+      },
+      createFilter(queryString) {
+        return (restaurant) => {
+          return (restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+        };
+      },
   }
 }
 </script>
