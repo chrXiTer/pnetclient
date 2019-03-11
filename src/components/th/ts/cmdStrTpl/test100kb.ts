876123aeba475d9c8e4af5bb4424e95d21839a0f
chrXiTer <1026336374@qq.com>
2019-03-11 17:45:21 +0800

    123

--- src/components/th/ts/cmdStrTpl/test100kb.ts
+++ src/components/th/ts/cmdStrTpl/test100kb.ts
@@ -37,17 +37,16 @@ let test100kb = {
 getCmdCreateBridgeNet(host:string, hosts:string[]){
     let subnetPrefixs = HostToSubnetPrefix[host]
     // host本机创建网桥的命令
-    let c = subnetPrefixs.map((subnetPrefix, index,)=>{return `
+    let c = subnetPrefixs.map((subnetPrefix, index)=>{return `
 docker network create --subnet=${subnetPrefix}0/20 --gateway=${subnetPrefix}1 netb${index+1};
         `.trim()}).join(' ')
 
-    // host本机添加到达其他主机相关网桥的路由命令
-    let r = hosts.map((ohost) =>{
-        if(ohost == host){
+    let r = hosts.map((ohost) =>{ // host本机添加到达其他主机相关网桥的路由命令
+        if(ohost == host){        // 本机无需添加
             return ''
         }else{
             let subnetPrefixs = HostToSubnetPrefix[ohost]
-            return subnetPrefixs.map((subnetPrefix, index,)=>{return `
+            return subnetPrefixs.map((subnetPrefix)=>{return `
 route add -net ${subnetPrefix}0/20  gw ${ohost};
             `.trim()}).join(' ')
         }
@@ -65,15 +64,11 @@ getCmdDeleteBridgeNet(usedNetName:string, host:string, hosts:string[]){
     let c = `
 docker network rm ${usedNetName};
 `.trim()
-    // host本机删除到达其他主机对应网桥的路由命令
+    // host本机中，删除到达其他主机对应网桥的路由命令
     let r = hosts.map((ohost) =>{
-        if(ohost == host){
-            return ''
-        }
+        if(ohost == host){return '' }  // 本机有该类路由，无需删除
         let netNo = Number.parseInt((usedNetName.match(/\d+/) || ["-1"])[0])
-        if(netNo < 0){
-            return ''
-        }
+        if(netNo < 0){return ''}       // 网络名有误，不执行删除命令
         let subnetPrefix = HostToSubnetPrefix[ohost][netNo - 1]
         return `
 route del -net ${subnetPrefix}0/20;
