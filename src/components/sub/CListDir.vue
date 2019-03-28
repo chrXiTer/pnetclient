3aa1a7bd6004d3dc6c2200ae46b3f5baa863e295
chrXiTer <1026336374@qq.com>
2019-03-28 11:44:37 +0800

    123

--- src/components/sub/CListDir.vue
+++ src/components/sub/CListDir.vue
@@ -26,7 +26,7 @@ var dataTree = [{
 
 function handlerRetStr(s_this, resp, dataObj, node, nodeComponent){
     const newData = resp.data
-    for(let item of newData.nodes){
+    for(let item of newData){
         dataObj.children.push({
             id: id,
             key: dataObj.key + "/" + item,
