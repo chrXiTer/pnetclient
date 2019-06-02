e378c7f4f03f7526f2dc07464c99a12afa218121
chrXiTer <thanyouqdx@live.cn>
2019-06-02 19:44:30 +0800

    123
    123

--- src/components/PageAside.vue
+++ src/components/PageAside.vue
@@ -1,16 +1,14 @@
 <template>
   <el-menu :default-openeds="['1']" unique-opened router>
-    <el-submenu index="/">
-      <template slot="title"><i class="el-icon-message"></i>测试</template>
-        <el-menu-item index="/table">测试-table</el-menu-item>
-        <el-menu-item index="/home">测试-home</el-menu-item>
-        <el-menu-item index="/ssh">系统环境初始化</el-menu-item>
-        <el-menu-item index="/ssh2">测试1</el-menu-item>
-    </el-submenu>
+
     <el-submenu index="2">
       <template slot="title"><i class="el-icon-menu"></i>TH</template>
       <el-menu-item-group>
         <template slot="title">th</template>
+        <el-menu-item index="/table">主机管理</el-menu-item>
+        <el-menu-item index="/home">容器管理</el-menu-item>
+        <el-menu-item index="/ssh">容器镜像管理</el-menu-item>
+        <el-menu-item index="/ssh2">测试1</el-menu-item>
         <el-menu-item index="/th">ThMain</el-menu-item>
         <el-menu-item index="/docker">ThDocker</el-menu-item>
         <el-menu-item index="/etcdTree">ThEtcdTree</el-menu-item>
@@ -25,11 +23,21 @@
         <el-menu-item index="2-4-1">选项4-1</el-menu-item>
       </el-submenu>
     </el-submenu>
+  </el-menu>
+</template>
+
+<!--
+      <el-submenu index="/">
+      <template slot="title"><i class="el-icon-message"></i>测试</template>
+        <el-menu-item index="/table">测试-table</el-menu-item>
+        <el-menu-item index="/home">测试-home</el-menu-item>
+        <el-menu-item index="/ssh">系统环境初始化</el-menu-item>
+        <el-menu-item index="/ssh2">测试1</el-menu-item>
+    </el-submenu>
     <el-submenu index="3">
       <template slot="title"><i class="el-icon-menu"></i>用户权限管理</template>
       <el-menu-item index="/adduser">用户列表</el-menu-item>
       <el-menu-item index="/adduser">添加用户</el-menu-item>
     </el-submenu>
     <el-menu-item index="/th"><i class="el-icon-message"></i>ThMain</el-menu-item>
-  </el-menu>
-</template>
+-->
