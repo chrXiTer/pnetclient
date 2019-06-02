8fde12fdc1443c4cf03f53893c240df0b81bd031
chrXiTer <thanyouqdx@live.cn>
2019-06-02 23:29:32 +0800

    123
    123

--- src/components/PageAside.vue
+++ src/components/PageAside.vue
@@ -2,27 +2,37 @@
   <el-menu :default-openeds="['1']" unique-opened router>
 
     <el-submenu index="1">
-      <template slot="title"><i class="el-icon-menu"></i>TH</template>
+      <template slot="title"><i class="el-icon-menu"></i>容器管理系统</template>
       <el-menu-item-group>
-        <template slot="title">th</template>
-        <el-menu-item><a href="http://localhost:9040/" style="text-decoration:unset;color:black" target="_blank">主机管理</a></el-menu-item>
-        <el-menu-item><a href="http://localhost:9040/" style="text-decoration:unset;color:black" target="_blank">容器管理</a></el-menu-item>
+        <!--
+        <el-menu-item><a href="http://10.145.0.8:8080/env/1a5/infra/hosts" style="text-decoration:unset;color:black" target="_blank">主机管理</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8080/env/1a5/infra/containers" style="text-decoration:unset;color:black" target="_blank">容器管理</a></el-menu-item>
         <el-menu-item index="/scope">主机管理2</el-menu-item>
-        <el-menu-item index="/ssh">容器镜像管理</el-menu-item>
-        <el-menu-item index="/ssh2">测试1</el-menu-item>
         <el-menu-item index="/th">ThMain</el-menu-item>
-        <el-menu-item index="/docker">ThDocker</el-menu-item>
-        <el-menu-item index="/etcdTree">ThEtcdTree</el-menu-item>
-        <el-menu-item index="/thHostsMino">thHostsMino</el-menu-item>
-        <el-menu-item index="/k8s">k8s</el-menu-item>
+        http://10.145.0.8:8000/env/1a5/infra/container-log?instanceId=1i17206&isPopup=true
+        <el-menu-item index="/thHostsMino">系统日志</el-menu-item>
+        <el-menu-item index="/docker">系统设置</el-menu-item>
+        <el-menu-item index="/th">镜像管理</el-menu-item>
+        <el-menu-item index="/etcdTree">存储管理</el-menu-item>
+        
+        <el-menu-item><a href="http://localhost:9080/env/1a5/infra/hosts" style="text-decoration:unset;color:black" target="_blank">主机管理</a></el-menu-item>
+        <el-menu-item><a href="http://localhost:9080/env/1a5/infra/containers" style="text-decoration:unset;color:black" target="_blank">容器管理</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/admin/settings" style="text-decoration:unset;color:black" target="_blank">系统设置</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/env/1a5/infra/storagepools/pools" style="text-decoration:unset;color:black" target="_blank">存储管理</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/env/1a5/infra/container-log?instanceId=1i17206&isPopup=true" style="text-decoration:unset;color:black" target="_blank">系统日志</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/env/1a5/infra/registries" style="text-decoration:unset;color:black" target="_blank">系统日志</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/admin/processes" style="text-decoration:unset;color:black" target="_blank">系统进程</a></el-menu-item>
+                -->
+
+        <el-menu-item><a href="http://10.145.0.8:8000/env/1a5/infra/hosts" style="text-decoration:unset;color:black" target="_blank">主机管理</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/env/1a5/infra/containers" style="text-decoration:unset;color:black" target="_blank">容器管理</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/admin/settings" style="text-decoration:unset;color:black" target="_blank">系统设置</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/env/1a5/infra/storagepools/pools" style="text-decoration:unset;color:black" target="_blank">存储管理</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/env/1a5/infra/container-log?instanceId=1i17206&isPopup=true" style="text-decoration:unset;color:black" target="_blank">系统日志</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/env/1a5/infra/registries" style="text-decoration:unset;color:black" target="_blank">系统日志</a></el-menu-item>
+        <el-menu-item><a href="http://10.145.0.8:8000/admin/processes" style="text-decoration:unset;color:black" target="_blank">系统进程</a></el-menu-item>
+
       </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="2-3">选项3</el-menu-item>
-      </el-menu-item-group>
-      <el-submenu index="2-4">
-        <template slot="title">选项4</template>
-        <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-      </el-submenu>
     </el-submenu>
   </el-menu>
 </template>
@@ -41,4 +51,16 @@
       <el-menu-item index="/adduser">添加用户</el-menu-item>
     </el-submenu>
     <el-menu-item index="/th"><i class="el-icon-message"></i>ThMain</el-menu-item>
+
+
+    <el-menu-item index="/k8s">k8s</el-menu-item>
+
+      <el-menu-item-group title="分组2">
+        <el-menu-item index="2-3">选项3</el-menu-item>
+      </el-menu-item-group>
+      <el-submenu index="2-4">
+        <template slot="title">选项4</template>
+        <el-menu-item index="2-4-1">选项4-1</el-menu-item>
+      </el-submenu>
+
 -->
--- src/components/th/ThScope.vue
+++ src/components/th/ThScope.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    dddddddddddddddd
     <iframe src="http://localhost:9040/" width="100%" height="800px"></iframe>
   </div>
 </template>
