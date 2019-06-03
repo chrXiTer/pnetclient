cc7a83c0aec075f96f0210ba492b63d2d3e043a9
chrXiTer <thanyouqdx@live.cn>
2019-06-03 09:17:15 +0800

    123
    123

--- src/components/th/THR7.vue
+++ src/components/th/THR7.vue
@@ -1,5 +1,26 @@
 <template>
   <div>
-    <iframe src="http://10.145.0.8:8000/admin/processes" width="100%" height="800px"></iframe>
+    <iframe id="external-frame" src="http://10.145.0.8:8000/admin/processes" width="100%" height="800px"></iframe>
   </div>
 </template>
+
+
+  <script>
+export default {
+  mounted: function () {
+    setIframeHeight(document.getElementById('external-frame'));
+  }
+}
+    function setIframeHeight(iframe) {
+      if (iframe) {
+        var iframeWin = iframe.contentWindow
+        if (iframeWin.document.body) {
+          iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
+        }
+      }
+    };
+
+
+  </script>
+
+
